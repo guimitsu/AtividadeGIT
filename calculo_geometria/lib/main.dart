d64c030ca5bf6b68e7c0f3b23771d510b22e5c98
guimitsu <guilherme.shigematu@fatec.sp.gov.br>
2021-05-02 23:05:03 -0300

    Criado os botões do menu inicial

--- calculo_geometria/lib/main.dart
+++ calculo_geometria/lib/main.dart
@@ -4,4 +4,26 @@ void main() {
   runApp(AppWidget());
 }
 
-class AppWidget extends StatelessWidget {}
+class AppWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Cálculo de Área e perímetro'),
+        ),
+        body: Container(
+          alignment: Alignment.center,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              ElevatedButton(onPressed: () {}, child: Text('Círculo')),
+              ElevatedButton(onPressed: () {}, child: Text('Retângulo')),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
